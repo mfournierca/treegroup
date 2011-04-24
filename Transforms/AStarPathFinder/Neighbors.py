c11a3cec4f325d8953e1e3c0b36c2c5120d1c51d
matt <matt@10.0.1.200>
2011-04-24 09:32:50 -0400

    Added accessor methods to the neighbor class

--- Transforms/AStarPathFinder/Neighbors.py
+++ Transforms/AStarPathFinder/Neighbors.py
@@ -18,8 +18,41 @@ class Neighbor:
         self.hscore = None
         self.fscore = None
         self.camefrom = None
+        
         self.dist = None
-        self.tree = None
+        
+        self._tree = None
+        self._operand = None
+    
+    
+    #===========================================================================
+    # accessor methods
+    #===========================================================================
+    
+    #note: since the AStarPathFinder will be used to transform trees, 
+    #we have to be able to handle large trees. Some documents get up to 
+    #5 MB, with hundreds of elements - if we stored the full tree of each neighbor
+    #in memory, we would reach GB quickly. Therefore, it will be necessary at some 
+    #point to do something more clever, ie only store the operand and only
+    #calculate the full neighbor tree when needed, or write the trees to disk 
+    #and retrieve them when needed. 
+    #If in the future it is necessary to do this, it should be done here by changing
+    #the accessor methods to write or read to disk, or calculate the neighbor 
+    #tree as needed. These accessor methods are already created, although rather 
+    #simple at this point. But changing them in the future will not require changes
+    #in any other part of the program. 
+        
+    def getOperandTree(self):
+        return self._operand
+    
+    def setOperandTree(self, tree):
+        self._operand = tree
+    
+    def getTree(self):
+        return self._tree
+    
+    def setTree(self, tree):
+        self._tree = tree
         
     
     
@@ -59,7 +92,7 @@ def findNeighbors_FirstValidationError(sourcetree):
     for  o in operands:
         neighbor = Neighbor()
 #        neighbor.operand = copy.deepcopy(o.tree)
-        neighbor.tree = Tree.Tree.add(o.tree, sourcetree)
+        neighbor.setTree(Tree.Tree.add(o.tree, sourcetree))
         log.debug('\tneighbor: %s' % lxml.etree.tostring(neighbor.tree))
         neighbors.append(neighbor)
         
