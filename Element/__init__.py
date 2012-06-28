3688f3adaa9be796d8bc43103feda2afebe355d4
matt <fournier.matthew@gmail.com>
2012-06-28 12:30:49 -0400

    added license notice to __init__ files

---
+++ Element/__init__.py
@@ -0,0 +1,16 @@
+  # Copyright  (c) 2012 Samalander-OS Ltd.  
+  # See the file "License-EULA-Samalander-Software-Center.txt" at the root 
+  # level of the Samalander Software Center installation directory for 
+  # licensing terms applicable Samalander-OS Ltd. software including the 
+  # Samalander Software Center and all associated software modules.  
+  #
+  # Other software in this package  is licensed to you under the software's 
+  # own applicable license terms and conditions. 
+  # A list of these licenses can be found in at the root 
+  # level of the Samalander Software Center installation directory in the
+  # THIRD-PARTY-LICENSES.txt file, the documentation or, as
+  # applicable, third-party licenses document at
+  # "http://www.samalander.com/Content/About/Legal/THIRD-PARTY-LICENSES.html or
+  # "http://www.samalander.com/Content/About/Legal/THIRD-PARTY-LICENSES.txt".
+
+
